97dcc633161d41e6620c46dcabf494cb4c76ffe6
kublahan <67871033+kublahan@users.noreply.github.com>
2023-09-01 22:10:11 +0300

    Add Cart functions

--- Rocky/Rocky/WC.cs
+++ Rocky/Rocky/WC.cs
@@ -3,5 +3,6 @@
     public static class WC//Веб константа
     {
         public static string ImagePath = @"\images\product\";
+        public static string SessionCart = "ShoppingCartSession";
     }
 }
