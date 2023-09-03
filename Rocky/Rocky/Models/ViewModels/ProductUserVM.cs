fa534b98fa227e66124a2bbae92abc613724ec42
kublahan <67871033+kublahan@users.noreply.github.com>
2023-09-03 17:03:52 +0300

    Add Cart delete and view

---
+++ Rocky/Rocky/Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Rocky.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ProductUserVM()
+        {
+            ProductList = new List<Product>();
+        }
+
+
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<Product> ProductList { get; set; }
+    }
+}
