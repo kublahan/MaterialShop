06aa9ac4d6f79cf7ef871f5e989c529913f3fc86
kublahan <67871033+kublahan@users.noreply.github.com>
2024-02-11 14:02:39 +0300

    Update SDL version

--- Rocky/Rocky/Data/ApplicationDbContext.cs
+++ Rocky/Rocky/Data/ApplicationDbContext.cs
@@ -13,7 +13,6 @@ namespace Rocky.Data
         }
 
         public DbSet<Category> Category { get; set; }
-        public DbSet<ModelHomeWork> ModelHomeWork { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
     }
